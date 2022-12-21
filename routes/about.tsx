1b8fadbd666313e9f9fa3f47fd61b41d85eb36ea
01Sergiu <68728921+Sergiu1-cyber@users.noreply.github.com>
2022-12-21 23:13:35 +0000

    Nav

--- routes/about.tsx
+++ routes/about.tsx
@@ -2,9 +2,13 @@
 import { h } from "preact";
 import { tw } from "@twind";
 
+import TheNavigation from "../components/TheNavigation.tsx"
+
+
 export default function About() {
   return (
     <div>
+      <TheNavigation />
       <h1>About Page</h1>
       <h3>Deno e Cool</h3>
     </div>
